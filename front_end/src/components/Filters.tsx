089894e58b0a3410bf29bbcd010f0145d186b522
Ricardo Medina <medina.r.dev@gmail.com>
2024-06-14 02:27:49 -0400

    feat: headers and filters ui v0.1

--- front_end/src/components/Filters.tsx
+++ front_end/src/components/Filters.tsx
@@ -1,6 +1,6 @@
 export function Filters() {
    return (
-      <div className="mt-4 flex justify-center gap-4 text-gray-800 text-sm font-semibold">
+      <div className="mt-4 flex justify-center gap-4 text-zinc-600 text-sm font-semibold">
         <button className="border bg-white px-4 py-2 rounded-md">Apartamento</button>
         <button className="border bg-white px-4 py-1 rounded-md">Venta</button>
         <button className="border bg-white px-4 py-1 rounded-md">Precio</button>
--- front_end/src/components/Header.tsx
+++ front_end/src/components/Header.tsx
@@ -1,18 +1,33 @@
+
 import Logo from "../../public/logo.png"
 
 export function Header() {
 
-   const inputStyle = {
-      maxWidth: "500px",
-   }
-
    return (
       <div className="flex gap-10 items-center justify-between mt-2">
          <div aria-label="logo group" className="flex gap-1 items-center ml-4">
-            <img className= "size-8" src={Logo} alt="Fellow MLS logo" />
+            <img className="size-8" src={Logo} alt="Fellow MLS logo" />
             <div className="text-lg text-blue-500 font-bold tracking-tighter logo-font">Fellow MLS</div>
          </div>
-         <input aria-label="text" className="flex-1 border-gray-400 border rounded-md text-lg text-gray-700 py-2 px-4 placeholder:text-sm focus:border-dark-blue focus:outline-none" style={inputStyle} placeholder="Urbanizacion, Precio, Detalles..." />
+
+         <div aria-label="search bar" className="
+            flex-1 max-w-[500px] flex items-center
+            py-2 px-4
+            bg-zinc-100 border border-zinc-300 rounded-md
+         ">
+            <input type="text" className="
+               flex-1
+               bg-transparent
+               text-lg text-zinc-700
+               placeholder:text-sm placeholder:text-zinc-400
+               focus:outline-none
+            " placeholder="Urbanizacion, Precio, Detalles..." onFocus={onFocusInput}/>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 bg-transparent stroke-zinc-400 stroke-2">
+               <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+            {/* <div className="flex items-center">
+            </div> */}
+         </div>
          <div aria-label="right links" className="flex gap-6 mr-6 text-gray-500 text-sm">
             <a className="hover:text-blue-800" href="/about">About</a>
             <a className="hover:text-blue-800" href="/help">Help</a>
@@ -20,4 +35,10 @@ export function Header() {
          </div>
       </div>
    )
+
+   //
+   function onFocusInput(ev: React.FocusEvent<HTMLInputElement, Element>) {
+      // todo: div of parent
+        // focus:border-dark-blue focus:border focus:outline-none
+   }
 }
